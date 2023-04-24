360d716104f995cc0c4553456f9b850cde777cc3
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-04-25 00:07:26 +0530

    Code reafactor remove student

--- ch2-router/routes/student.js
+++ ch2-router/routes/student.js
@@ -1,22 +1,22 @@
 import express from 'express'
 const router = express.Router()
 
-router.get('/student/all', (req, res) =>{
+router.get('/all', (req, res) =>{
     res.send('all student')
 })
 
 
-router.post('/student/create', (req, res) =>{
+router.post('/insert', (req, res) =>{
     res.send('Creation of new student')
 })
 
 
-router.put('/student/update', (req, res) =>{
+router.put('/update', (req, res) =>{
     res.send('Update student')
 })
 
 
-router.delete('/student/delete', (req, res) =>{
+router.delete('/delete', (req, res) =>{
     res.send('Delete student')
 })
 
