ba924235972d8d942837a7525fcb967d1bc17949
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-04-24 23:04:17 +0530

    docs: comment in code for better understanding

--- ch0/app.js
+++ ch0/app.js
@@ -1,12 +1,18 @@
-import express from 'express'
+import express from 'express' // ES 6
+//  const express from 'express' // Old way
 
+// Express Function
 const app = express ()
 
+// Port definition
 const port = process.env.PORT || '3000'
 
+// Root Creates
 app.get('/', ( req, res ) =>{
     res.send('Namaste Express.js')
 })
+
+// Listening Server design
 
 app.listen(port, ()=>{
     console.log(` Server listening at http://localhost:${port}`)
