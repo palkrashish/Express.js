8729a4ce65ad57ab59af444dd3781bde907eb388
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-04-27 14:47:08 +0530

    best way to write a connecting without authentication

--- ch4- Mongoose/app.js
+++ ch4- Mongoose/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 const port = process.env.PORT || "3000";
 
-const DATABASE_URL = "mongodb://127.0.0.1:27017/schholdb";
+const DATABASE_URL = "mongodb://127.0.0.1:27017";
 connectdb(DATABASE_URL);
 app.get("/", (req, res) => {
   res.send("hello");
--- ch4- Mongoose/db/connectdb.js
+++ ch4- Mongoose/db/connectdb.js
@@ -2,8 +2,12 @@ import mongoose from "mongoose";
 
 const connectdb = async (DATABASE_URL) => {
   try {
+    const DB_OPTIONS = {
+        dbName: 'schooldb',
+        authSource: 'schooldb'
+    }
         await mongoose
-            .connect(DATABASE_URL);
+            .connect(DATABASE_URL,DB_OPTIONS);
         console.log("Connect DB Sucessfully");
     } catch (err) {
         console.log(err);
