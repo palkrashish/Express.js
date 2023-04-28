e621e4c3034b29bebf83b1ebe0244b4a06f15764
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-04-28 15:24:11 +0530

    Feat: for multiple insert into document

--- ch5-schema and model/models/Student.js
+++ ch5-schema and model/models/Student.js
@@ -29,18 +29,35 @@ const createDoc = async (nm, ag, fe, hob, isact, comt) => {
 
     // CREATING A NEW DOCUMENT
 
-    const studentDoc = new studentModel({
-      name: nm,
-      age: ag,
-      fees: fe,
-      hobbies: hob,
-      isActive: isact,
-      comments: comt,
-    });
+    // const studentDoc = new studentModel({
+    //   name: nm,
+    //   age: ag,
+    //   fees: fe,
+    //   hobbies: hob,
+    //   isActive: isact,
+    //   comments: comt,
+    // });
 
+   
+    const studentDoc = new studentModel({
+        name: "Ajay",
+        age: 49,
+        fees: 8500.40,
+        hobbies: ["enjoy, Cricket, Singing"],
+        isActive: true,
+        comments: [{ value: "This is good mongoose" }],
+      });
+      const studentDoc1 = new studentModel({
+        name: "Sangita",
+        age: 40,
+        fees: 5500.40,
+        hobbies: ["Basketball, game"],
+        isActive: true,
+        comments: [{ value: "Learning Js" }],
+      });
     // Saving Document
 
-    const result = await studentDoc.save();
+    const result = await studentModel.insertMany([studentDoc, studentDoc1]);
     console.log(result);
   } catch (error) {
     console.log(error);
