22bee99fb4cb345e7fd4e5c1a04702008c218dfe
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-04-28 14:56:44 +0530

    Feat:- creating a new document and saving document into DATABASE

--- ch5-schema and model/app.js
+++ ch5-schema and model/app.js
@@ -5,8 +5,11 @@ const port = process.env.PORT || '3000'
 import connectdb from "./db/connectdb.js";
 
 const DABASE_URL = 'mongodb://127.0.0.1/27017'
-import './models/Student.js'
+import createDoc from  "./models/Student.js";
 connectdb(DABASE_URL)
+
+//Create and Save Document
+createDoc()
 
 app.get('/', (req, res)=>{
     res.send('hello dear')
--- ch5-schema and model/models/Student.js
+++ ch5-schema and model/models/Student.js
@@ -1,19 +1,51 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+
+const createDoc = async () => {
+  try {
+    // DEFINING SCHEMA
+
+    const studentSchema = new mongoose.Schema({
+      name: { type: String, required: true, trim: true },
+      age: { type: Number, required: true, min: 18, max: 50 },
+      fees: {
+        type: mongoose.Decimal128,
+        required: true,
+        validate: (v) => {
+          v >= 5500.50;
+        },
+      },
+      hobbies: { type: Array },
+      isActive: { type: Boolean },
+      comments: [
+        { value: { type: String }, publish: { type: Date, default: Date.now } },
+      ],
+      join: { type: Date, default: Date.now },
+    });
+    // console.log(studentSchema.path('age')) // for know the path which are required
+
+    // COMPILING SCHEMA
+
+    const studentModel = mongoose.model("student", studentSchema);
+
+    // CREATING A NEW DOCUMENT
+
+    const studentDoc = new studentModel({
+      name: "Ranjit",
+      age: 49,
+      fees: 8500.40,
+      hobbies: ["enjoy, Cricket, Singing"],
+      isActive: true,
+      comments: [{ value: "This is good mongoose" }],
+    });
+
+    // Saving Document
+
+    const result = await studentDoc.save();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 
-// DEFINING SCHEMA
-
-const studentSchema = new mongoose.Schema ({
-    name: {type: String, required: true, trim :true},
-    age: {type: Number, required: true, min: 18, max: 50},
-    fess: {type: mongoose.Decimal128, required: true, validate:((v)=>{ v>= 5500.50})},
-    hobbies: {type: Array},
-    isActive: {type: Boolean},
-    comments: [{value: {type: String}, publish: {type: Date, default: Date.now}}],
-    join: {type : Date, default: Date.now}
-})
-// console.log(studentSchema.path('age')) // for know the path which are required 
-
-// COMPILING SCHEMA
-
-const studentModel = mongoose.model('student', studentSchema)
+export default createDoc
